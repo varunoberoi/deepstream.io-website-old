cb69f6a96378c704976f24495f774ed64e714898
Yasser Fadl <yasser.fadl@gmail.com>
2016-01-09 20:15:42 +0100

    Adding correct hightlight hover details on message specs

--- htdocs/assets/js/messagestructure.js
+++ htdocs/assets/js/messagestructure.js
@@ -1,41 +1,93 @@
 ( function( $ ) {
 
-	function formatSpecMessage( message ) {
-		var parts = message.split( '|' );
-		var html = "";
-		var	messageClass = "";
-		messageClass += parts[ 1 ] !== 'A' ? '' : ' ack-message-format';
-		messageClass += parts[ 1 ] !== 'E' ? '' : ' error-message-format';
+	var C = {
+	    TYPES: {
+	        'S': 'TYPES.STRING',
+	        'O': 'TYPES.OBJECT',
+	        'N': 'TYPES.NUMBER',
+	        'L': 'TYPES.NULL',
+	        'T': 'TYPES.TRUE',
+	        'F': 'TYPES.FALSE',
+	        'U': 'TYPES.UNDEFINED'
+	    },
+	    TOPIC: {
+	        'A': 'TOPIC.AUTH',
+	        'X': 'TOPIC.ERROR',
+	        'E': 'TOPIC.EVENT',
+	        'R': 'TOPIC.RECORD',
+	        'P': 'TOPIC.RPC',
+	        'W': 'TOPIC.WEBRTC',
+	        'PRIVATE/': 'TOPIC.PRIVATE'
+	    },
+	    EVENT: {
+	        'ACK_TIMEOUT': 'EVENT.ACK_TIMEOUT',
+	        'RESPONSE_TIMEOUT': 'EVENT.RESPONSE_TIMEOUT',
+	        'DELETE_TIMEOUT': 'EVENT.DELETE_TIMEOUT',
+	        'UNSOLICITED_MESSAGE': 'EVENT.UNSOLICITED_MESSAGE',
+	        'MESSAGE_PARSE_ERROR': 'EVENT.MESSAGE_PARSE_ERROR',
+	        'VERSION_EXISTS': 'EVENT.VERSION_EXISTS',
+	        'NOT_AUTHENTICATED': 'EVENT.NOT_AUTHENTICATED',
+	        'LISTENER_EXISTS': 'EVENT.LISTENER_EXISTS',
+	        'NOT_LISTENING': 'EVENT.NOT_LISTENING',
+	        'TOO_MANY_AUTH_ATTEMPTS': 'EVENT.TOO_MANY_AUTH_ATTEMPTS',
+	        'IS_CLOSED': 'EVENT.IS_CLOSED',
+	        'UNKNOWN_CALLEE': 'EVENT.UNKNOWN_CALLEE'
+	    },
+	    ACTIONS: {
+	        'A': 'ACTIONS.ACK',
+	        'R': 'ACTIONS.READ',
+	        'C': 'ACTIONS.CREATE',
+	        'U': 'ACTIONS.UPDATE',
+	        'P': 'ACTIONS.PATCH',
+	        'D': 'ACTIONS.DELETE',
+	        'S': 'ACTIONS.SUBSCRIBE',
+	        'US': 'ACTIONS.UNSUBSCRIBE',
+	        'I': 'ACTIONS.INVOKE',
+	        'SP': 'ACTIONS.SUBSCRIPTION_FOR_PATTERN_FOUND',
+	        'SR': 'ACTIONS.SUBSCRIPTION_FOR_PATTERN_REMOVED',
+	        'L': 'ACTIONS.LISTEN',
+	        'UL': 'ACTIONS.UNLISTEN',
+	        'PU': 'ACTIONS.PROVIDER_UPDATE',
+	        'Q': 'ACTIONS.QUERY',
+	        'CR': 'ACTIONS.CREATEORREAD',
+	        'EVT': 'ACTIONS.EVENT',
+	        'E': 'ACTIONS.ERROR',
+	        'REQ': 'ACTIONS.REQUEST',
+	        'RES': 'ACTIONS.RESPONSE',
+	        'REJ': 'ACTIONS.REJECTION'
+	    }
+	};
 
-		for( var i=0; i<parts.length; i++ ) {
-			html+= '<span class="message-format-part-' + i + '">' + parts[ i ] + '</span>';
-			if( i<parts.length-1) {
-				html += '<span class="message-format-pipe">|</span>';
-			}	
+	function parseMessageSpec( message ) {
+		var parts = message.substring( 0, message.length - 1 ).split( '|' );
+		var data = parts.slice( 2 );
+		for( var i=0; i<data.length;i++) {
+			data[ i ] = C.ACTIONS[ data[ i ] ] || C.EVENT[ data[ i ] ] || data[ i ];
 		}
-		html = html.replace( '+', '' );
-		html += '<span class="message-format-end">+</span>';
 		return {
-			class: messageClass,
-			content: html
+			raw: message,
+			topic: C.TOPIC[ parts[ 0 ] ],
+			action: C.ACTIONS[ parts[ 1 ] ],
+			data: data
 		}
 	}
-
-	Prism.hooks.add('wrap', function(env) {
-		if (env.type === 'msgSpec' && env.content.indexOf( '<' ) === -1 ) {
-			var messageSpec = formatSpecMessage( env.content );
-			env.content = messageSpec.content;
-			env.classes.push( messageSpec.class )
-		}
-	});
 
 	$( document ).ready(function() {
 
 		$( 'code' ).on( 'mouseenter', '.message-format', function( e ) {
-			var destination = $( e.target ).parent( '.message-format' ).offset();
+			var destination = $( e.target ).offset();
+			var message = parseMessageSpec( $( e.target).text() );
 
+			$( '.message-in-depth .event' ).text( message.topic );
+			$( '.message-in-depth .action' ).text( message.action );
+
+			var ml = message.data.length > 1 ? '</br>' : '';
+			$( '.message-in-depth .data' ).html( '[ ' + ml + message.data.join( ',</br>' ) + ml + ' ]' );
+
+			var height = $( '.message-in-depth' ).height() + 10;
+			var width = $( '.message-in-depth' ).width();
 			$( '.message-in-depth' )
-				.css({top: destination.top - 133, left: destination.left - 100 })
+				.css({top: destination.top - height, left: destination.left - (width/2) })
 				.show();
 		} );
 
